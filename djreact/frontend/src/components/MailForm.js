b4d07171f65aa9075b4153b0ea57c8964102dc2f
sverkausijluch <sverkausijluc@gmail.com>
2023-07-26 10:32:58 +0300

    ^^

--- djreact/frontend/src/components/MailForm.js
+++ djreact/frontend/src/components/MailForm.js
@@ -2,30 +2,33 @@ import React from 'react'
 import CSRFToken from "../csrftoken"
 import { useState } from 'react'
 
-export default function CommentForm(props) {
+export default function MailForm(props) {
     const [name, setName] = useState('')
     const [text, setText] = useState('')
+    const [email, setEmail] = useState('')
     const [result, setResult] = useState('')
     const updateName = (e) => {
         setName(e.target.value)
+    }
+    const updateEmail = (e) => {
+        setEmail(e.target.value)
     }
     const updateText = (e) => {
         setText(e.target.value)
     }
     const sendForm = (e) => {
         e.preventDefault()
-        if(text!=''&&name!='') {
+        if(text!=''&&name!=''&&email!='') {
             const formData = new FormData(document.querySelector('form'))
-            formData.append('article_id', props.article_id)
             $.ajax({
                 type: 'post',
-                url: '../api/create-comment',
+                url: '/api/send-mail',
                 cache: false,
                 data: formData,
                 processData: false,
                 contentType: false,
                 success: function (res) {
-                    setResult('Комментарий успешно отправлен!')
+                    setResult('Письмо успешно отправлено! Спасибо!')
                 },
                 error: function (xhr) {
                     console.log(JSON.parse(xhr.responseText))
@@ -34,19 +37,22 @@ export default function CommentForm(props) {
         }
     }
     return (
-        <form onSubmit={sendForm}>
+        <form onSubmit={sendForm} className="mail-form">
             {result !== '' &&
                 <p className="success-result">{result}</p>
             }
             <CSRFToken/>
             <div className="inputWrapper">
-                    <input value={name} onChange={updateName} placeholder="Имя" name="author_name"/>
+                <label>Имя</label>
+                    <input value={name} onChange={updateName} placeholder="" name="name"/>
             </div>
             <div className="inputWrapper">
-                    <input  value={name} onChange={updateName} placeholder="email" name="email"/>
+                <label>Почта</label>
+                    <input value={email} onChange={updateEmail} placeholder="" name="email"/>
             </div>
-				<div className="comment-textarea">
-					<textarea className="textarea" name="text" value={text} onChange={updateText}>Напишите здесь что-нибудь</textarea>
+				<div className="inputWrapper">
+                    <label>Сообщение</label>
+					<textarea name="text" className="textarea" value={text} onChange={updateText}></textarea>
 				</div>
                 <button type="submit" className="super-btn">
                     Отправить
--- djreact/frontend/src/components/MailPage.js
+++ djreact/frontend/src/components/MailPage.js
@@ -2,10 +2,31 @@ import React from 'react'
 import MainMenu from "./MainMenu";
 import MediaQuery from "react-responsive";
 import CiteHeader from "./CiteHeader";
+import MailForm from "./MailForm";
 
 class MailPage extends React.Component {
 	constructor(props) {
 		super(props)
+		this.sendForm = this.sendForm.bind(this)
+	}
+
+	sendForm = (e) => {
+		e.preventDefault()
+			const formData = new FormData(document.querySelector('form'))
+			$.ajax({
+				type: 'post',
+				url: '/api/send-mail',
+				cache: false,
+				data: formData,
+				processData: false,
+				contentType: false,
+				success: function (res) {
+					setResult('Письмо успешно отправлено! Спасибо!')
+				},
+				error: function (xhr) {
+					console.log(JSON.parse(xhr.responseText))
+				}
+			})
 	}
 
 	render() {
@@ -13,29 +34,15 @@ class MailPage extends React.Component {
 			<div className="main-container">
 				<MediaQuery maxWidth={800}>
 					<CiteHeader page="mail"/>
-    			</MediaQuery>
-    		    <MediaQuery minWidth={801}>
+				</MediaQuery>
+				<MediaQuery minWidth={801}>
 					<MainMenu page="mail"/>
 				</MediaQuery>
 				<div className="col-2">
 					<div className="mail-content">
 						<h2><i className="el-icon-chat-round"></i> Написать письмо</h2>
-						<form className="mail-form">
-							<div className="inputWrapper">
-								<label>Имя</label>
-								<input/>
-							</div>
-							<div className="inputWrapper">
-								<label>Почта</label>
-								<input/>
-							</div>
-							<div className="inputWrapper">
-								<label>Сообщение</label>
-								<textarea></textarea>
-							</div>
-							<button className="super-btn">Отправить</button>
-						</form>
-					</div>
+						<MailForm/>
+				</div>
 				</div>
 			</div>
 		)
