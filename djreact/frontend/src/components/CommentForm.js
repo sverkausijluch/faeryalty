ac6523e9d8b730b089e28df36209d99bf48b6ced
sverkausijluch <sverkausijluc@gmail.com>
2023-07-26 18:56:25 +0300

    ^^

--- djreact/frontend/src/components/CommentForm.js
+++ djreact/frontend/src/components/CommentForm.js
@@ -1,29 +1,38 @@
 import React from 'react'
 import CSRFToken from "../csrftoken"
 import { useState } from 'react'
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
+
+const schema = yup.object().shape({
+    name: yup
+        .string()
+        .max(50, 'Введите имя покороче')
+        .required("Заполните поле Имя"),
+    text: yup
+        .string()
+        .min(5, 'Текст слишком короткий')
+        .max(1000, 'Текст великоват, сократите его до 1000 символов')
+        .required("Введите текст сообщения"),
+})
 
 export default function CommentForm(props) {
-    const [name, setName] = useState('')
-    const [text, setText] = useState('')
     const [result, setResult] = useState('')
-    const updateName = (e) => {
-        setName(e.target.value)
-    }
-    const updateText = (e) => {
-        setText(e.target.value)
-    }
-    const sendForm = (e) => {
-        e.preventDefault()
-        if(text!=''&&name!='') {
-            const formData = new FormData(document.querySelector('form'))
-            formData.append('article_id', props.article_id)
+
+    const {register, handleSubmit, formState: {errors},} = useForm({
+        defaultValues: {name: '', text: ''},
+        mode: "onBlur",
+        resolver: yupResolver(schema),
+    })
+
+    const onSubmit = (data) => {
+            data.append('article_id', props.article_id)
             $.ajax({
                 type: 'post',
                 url: '../api/create-comment',
                 cache: false,
-                data: formData,
-                processData: false,
-                contentType: false,
+                data: data,
                 success: function (res) {
                     setResult('Комментарий успешно отправлен!')
                 },
@@ -31,23 +40,27 @@ export default function CommentForm(props) {
                     console.log(JSON.parse(xhr.responseText))
                 }
             })
-        }
     }
     return (
-        <form onSubmit={sendForm}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             {result !== '' &&
                 <p className="success-result">{result}</p>
             }
             <CSRFToken/>
             <div className="inputWrapper">
-                    <input value={name} onChange={updateName} placeholder="Имя" name="author_name"/>
+                    <input {...register("name")}/>
             </div>
 				<div className="comment-textarea">
-					<textarea className="textarea" name="text" value={text} onChange={updateText}></textarea>
+					<textarea {...register("text")}></textarea>
 				</div>
                 <button type="submit" className="super-btn">
                     Отправить
                 </button>
+
+            <div className="errors-block">
+                <p>{errors.name?.message}</p>
+                <p>{errors.text?.message}</p>
+            </div>
         </form>
     )
 }
